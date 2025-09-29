8283f1c7b2ec0bf880b4a4d957501598b727ac49
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2025-09-29 16:59:02 +0900

    Simplify launch file

--- realsense2_camera/launch/launch.py
+++ realsense2_camera/launch/launch.py
@@ -1,55 +1,60 @@
-from launch                  import LaunchDescription
-from launch.actions          import (DeclareLaunchArgument, OpaqueFunction,
-                                     GroupAction)
-from launch.substitutions    import (LaunchConfiguration, ThisLaunchFileDir,
-                                     PathJoinSubstitution, EqualsSubstitution,
-                                     IfElseSubstitution)
-from launch.conditions       import IfCondition, UnlessCondition
-from launch_ros.actions      import Node, LoadComposableNodes
-from launch_ros.descriptions import ComposableNode
+from launch                   import LaunchDescription
+from launch.actions           import (DeclareLaunchArgument, OpaqueFunction,
+                                      GroupAction)
+from launch.substitutions     import (LaunchConfiguration,
+                                      PathJoinSubstitution, EqualsSubstitution)
+from launch.conditions        import IfCondition, UnlessCondition
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions       import Node, LoadComposableNodes
+from launch_ros.descriptions  import ComposableNode
 
 launch_arguments = [
-    {'name':        'namespace',
-     'default':     '',
-     'description': 'namespace of the camera node'},
-    {'name':        'camera_name',
-     'default':     'd435',
-     'description': 'node name of the camera'},
-    {'name':        'config_file',
-     'default':     '',
-     'description': 'path to YAML file for configuring camera'},
-    {'name':        'external_container',
-     'default':     'false',
-     'description': 'use existing external container',
-     'choices':     ['true', 'false']},
-    {'name':        'container',
-     'default':     '',
-     'description': 'name of internal or external component container'},
-    {'name':        'vis',
-     'default':     'false',
-     'description': 'visualize camera outputs',
-     'choices':     ['true', 'false']},
-    {'name':        'log_level',
-     'default':     'info',
-     'description': 'debug log level',
-     'choices':     ['debug', 'info', 'warn', 'error', 'fatal']},
-    {'name':        'output',
-     'default':     'screen',
-     'description': 'pipe node output',
-     'choices':     ['screen', 'log', 'both']}]
-
-parameter_arguments = [
-    {'name':        'serial_no',
-     'default':     '',
-     'description': 'choose device by serial number'},
-    {'name':        'pointcloud.enable',
-     'default':     'true',
-     'description': 'enable publishing pointcloud',
-     'choices':     ['true', 'false']},
-    {'name':        'align_depth.enable',
-     'default':     'true',
-     'description': 'enable align depth filter',
-     'choices':     ['true', 'false']}]
+    {
+        'name':        'namespace',
+        'default':     '',
+        'description': 'namespace of the camera node'
+    },
+    {
+        'name':        'camera_name',
+        'default':     'd435',
+        'description': 'node name of the camera'
+    },
+    {
+        'name':        'config_file',
+        'default':     PathJoinSubstitution([
+                           FindPackageShare('realsense2_camera'), 'config',
+                           'default.yaml']),
+        'description': 'path to YAML file for configuring camera'
+    },
+    {
+        'name':        'external_container',
+        'default':     'false',
+        'description': 'use existing external container',
+        'choices':     ['true', 'false', 'True', 'False']
+    },
+    {
+        'name':        'container',
+        'default':     '',
+        'description': 'name of internal or external component container'},
+    {
+        'name':        'vis',
+        'default':     'false',
+        'description': 'visualize camera outputs',
+        'choices':     ['true', 'false', 'True', 'False']
+    },
+    {
+        'name':        'log_level',
+        'default':     'info',
+        'description': 'debug log level',
+        'choices':     ['debug', 'info', 'warn', 'error', 'fatal']
+    },
+    {
+        'name':        'output',
+        'default':     'screen',
+        'description': 'pipe node output',
+        'choices':     ['screen', 'log', 'both']
+    }
+]
 
 def declare_launch_arguments(args):
     return [DeclareLaunchArgument(arg['name'],
@@ -58,68 +63,62 @@ def declare_launch_arguments(args):
                                   choices=arg.get('choices')) \
             for arg in args]
 
-def set_configurable_parameters(args):
-    return {arg['name']: LaunchConfiguration(arg['name']) for arg in args}
-
-def launch_setup(context, param_args):
-    config_file   = IfElseSubstitution(
-                        EqualsSubstitution(
-                            LaunchConfiguration('config_file'), ''),
-                        PathJoinSubstitution([ThisLaunchFileDir(), '..',
-                                              'config', 'default.yaml']),
-                        LaunchConfiguration('config_file'))
-    config_params = set_configurable_parameters(param_args)
-    return [Node(namespace=LaunchConfiguration('namespace'),
-                 name=LaunchConfiguration('camera_name'),
-                 package='realsense2_camera',
-                 executable='realsense2_camera_node',
-                 parameters=[config_file, config_params],
-                 output=LaunchConfiguration('output'),
-                 arguments=['--ros-args', '--log-level',
-                            LaunchConfiguration('log_level')],
-                 emulate_tty=True,
-                 condition=IfCondition(
-                              EqualsSubstitution(
-                                  LaunchConfiguration('container'), ''))),
-            GroupAction(
-                condition=UnlessCondition(
-                              EqualsSubstitution(
-                                  LaunchConfiguration('container'), '')),
-                actions=[
-                    Node(name=LaunchConfiguration('container'),
-                         package='rclcpp_components',
-                         executable='component_container_mt',
-                         output=LaunchConfiguration('output'),
-                         arguments=['--ros-args', '--log-level',
-                                    LaunchConfiguration('log_level')],
-                         condition=UnlessCondition(
-                             LaunchConfiguration('external_container'))),
-                    LoadComposableNodes(
-                        target_container=LaunchConfiguration('container'),
-                        composable_node_descriptions=[
-                            ComposableNode(
-                                namespace=LaunchConfiguration('namespace'),
-                                name=LaunchConfiguration('camera_name'),
-                                package='realsense2_camera',
-                                plugin='realsense2_camera::RealSenseNodeFactory',
-                                parameters=[config_file, config_params],
-                                extra_arguments=[
-                                    {'use_intra_process_comms': True}])])]),
-            GroupAction(
-                condition=IfCondition(LaunchConfiguration('vis')),
-                actions=[
-                    Node(name='rviz', package='rviz2', executable='rviz2',
-                         output='screen',
-                         arguments=['-d',
-                                    PathJoinSubstitution([
-                                        ThisLaunchFileDir(),
-                                        [LaunchConfiguration('camera_name'),
-                                         '.rviz']])]),
-                    Node(name='rqt_reconfigure', package='rqt_reconfigure',
-                         executable='rqt_reconfigure', output='screen')])]
+def launch_setup(context):
+    return [
+        Node(namespace=LaunchConfiguration('namespace'),
+             name=LaunchConfiguration('camera_name'),
+             package='realsense2_camera',
+             executable='realsense2_camera_node',
+             parameters=[LaunchConfiguration('config_file')],
+             output=LaunchConfiguration('output'),
+             arguments=['--ros-args', '--log-level',
+                        LaunchConfiguration('log_level')],
+             emulate_tty=True,
+             condition=IfCondition(
+                           EqualsSubstitution(
+                               LaunchConfiguration('container'), ''))),
+        GroupAction(
+            condition=UnlessCondition(
+                          EqualsSubstitution(
+                              LaunchConfiguration('container'), '')),
+            actions=[
+                Node(name=LaunchConfiguration('container'),
+                     package='rclcpp_components',
+                     executable='component_container_mt',
+                     output=LaunchConfiguration('output'),
+                     arguments=['--ros-args', '--log-level',
+                                LaunchConfiguration('log_level')],
+                     condition=UnlessCondition(
+                                   LaunchConfiguration('external_container'))),
+                LoadComposableNodes(
+                    target_container=LaunchConfiguration('container'),
+                    composable_node_descriptions=[
+                        ComposableNode(
+                            namespace=LaunchConfiguration('namespace'),
+                            name=LaunchConfiguration('camera_name'),
+                            package='realsense2_camera',
+                            plugin='realsense2_camera::RealSenseNodeFactory',
+                            parameters=[LaunchConfiguration('config_file')],
+                            extra_arguments=[{'use_intra_process_comms': True}]
+                        )
+                    ])
+            ]),
+        GroupAction(
+            condition=IfCondition(LaunchConfiguration('vis')),
+            actions=[
+                Node(name='rviz', package='rviz2', executable='rviz2',
+                     output='screen',
+                     arguments=[
+                         '-d',
+                         PathJoinSubstitution([
+                             FindPackageShare('realsense2_camera'), 'launch',
+                             [LaunchConfiguration('camera_name'), '.rviz']])
+                     ]),
+                Node(name='rqt_reconfigure', package='rqt_reconfigure',
+                     executable='rqt_reconfigure', output='screen')
+            ])
+    ]
 
 def generate_launch_description():
-    return LaunchDescription(declare_launch_arguments(launch_arguments +
-                                                      parameter_arguments) + \
-                             [OpaqueFunction(function=launch_setup,
-                                             args=[parameter_arguments])])
+    return LaunchDescription(declare_launch_arguments(launch_arguments) + \
+                             [OpaqueFunction(function=launch_setup)])
